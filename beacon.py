b7a315755c9cdfae6d76a7f9821e1cd436011d6c
deadpix3l <skylerr.curtis@gmail.com>
2017-03-06 18:22:22 +0000

    consolidate beacon scripts

--- beacon.py
+++ beacon.py
@@ -1,19 +1,46 @@
 #!/usr/bin/env python
+import argparse
 import socket
 import time
+    
+parser = argparse.ArgumentParser(description='Beacon is a simple script to test connections.')
+parser.add_argument('host', help="the host to connect to", type=str, default="127.0.0.1")
+parser.add_argument('-s', '--server', help='Server mode [default: no]', action="store_true")
+parser.add_argument('-p', '--port', help='Port server is listening on', type=int, default=8080)
+parser.add_argument('-b', '--beacon', help='Text to send to server', type=str, default="beacon")
+args = vars(parser.parse_args())
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('127.0.0.1', 8080))
-print "Connected!"
+if args.server:
+    s.bind((args.host, args.port))
+    s.listen(1)
+    print "server started"
+    conn, addr = s.accept()
+    s.close()
+    print 'Connected by', addr
+    
+else:
+    s.connect((args.host, args.port))
+    conn = s
+    addr = args.host
+    print "Connected!"
+    
 try:
-	while 1:
-		time.sleep(5) # 10 seconds
-		s.sendall('BEACON SYN')
-		data = s.recv(1024)
-		if not data: break
-		print 'recvd:', repr(data)
-		
+    if args.server:
+        while True:
+            data = conn.recv(1024)
+            if not data: break
+            print "recvd: {0} \t reply: {0} ACK".format(data)
+            conn.sendall(data + ' ACK')
+            
+    else:
+        while True:
+            conn.sendall(args.beacon)
+            data = s.recv(1024)
+            if not data: break
+            print 'recvd: {}'.format(data)
+            
 except KeyboardInterrupt: pass
-finally: 
-	print "Done"
-	s.close()
+finally:
+    print 'Connection terminated', addr
+    conn.close()
