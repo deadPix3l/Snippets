a335962389a6daac49ff2483ac6c71cde50e8653
deadpix3l <skylerr.curtis@gmail.com>
2017-06-29 17:42:54 +0000

    added logging and error handling

--- blockchain-game.py
+++ blockchain-game.py
@@ -1,42 +1,74 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
+import threading
+import socket
+import logging
+
+logging.basicConfig(level=logging.INFO,
+                    format='[%(levelname)s] (%(threadName)-10s) %(message)s',
+                    )
 
 blockchain = ''
 queue = []
 
-class server(threading.thread):
+class Server(threading.Thread):
 
-    def init():
+    def __init__(self):
+        super(Server, self).__init__()
+        self.name = "Server"
         self.s = socket.socket()
+        self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.s.bind(('0.0.0.0', 31337))
         self.s.listen(5)
 
-    def run():
+    def run(self):
         while True:
-            conn, addr = s.accept()
+            conn, addr = self.s.accept()
+            t = threading.Thread(target=handleClient, args=(conn, addr))
+            t.start()
+            logging.debug("new thread created for {}".format(addr))
 
 
 def handleClient(sock, addr):        
-
-    request = sock.recv(4096)
-    command, _, params = request.partition(' ')
+    global blockchain
+    global queue
+    request = sock.recv(4096).rstrip()
+    logging.info("HOST: {}\tREQUEST: {} ".format(addr[0], request))
+    command, _, params = request.rstrip().partition(' ')
 
     if command == "DELTA":
         offset = int(params)
-        sock.send(blockchain[offset:])
-
+        sock.send(blockchain[offset:]+'\n')
+   
     elif command == "BLOCK":
         blockchain += params
+        logging.debug("Block added: {}".format(params))
+        sock.send("200 OK\n")
         
     elif command == "PLAY":
         try:
-            response = "CLIENT {}".format(queue.pop(0))
+            x = queue.pop(0)
+            response = "CLIENT {}:{}\n".format(x[0], x[1])
+            
         except IndexError:
-            response = "SERVER"
+            response = "SERVER {}\n".format(addr[1])
             queue.append(addr)
+            
+        except Exception as e:
+            response = "501 SERVER ERROR\n"
+            logging.error(e)
+
         finally:
             sock.send(response)
+            #logging.debug("CURRENT QUEUE: {}".format(queue))
 
     else:
-        sock.send("UNKNOWN COMMAND")
+        sock.send("UNKNOWN COMMAND\n")
+        logging.warning("Unknown Command: {}".format(command))
 
     sock.close()
+    logging.debug("Thread Exiting")
+
+if __name__ == "__main__":
+    server = Server()
+    server.start()
+    logging.info("Server started")
